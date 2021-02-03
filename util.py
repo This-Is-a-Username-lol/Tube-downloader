4c711fc8572fbedb1b9236e9f1437449953617c9
Ashraful Islam <ashraful@ashraful.eu>
2021-02-03 15:19:07 +0100

    update: simplify file delete logic, credits: @agentsmith6969, @keysemble from #Python

--- util.py
+++ util.py
@@ -9,9 +9,6 @@ log = get_logger('util.py')
 
 def delete_file(file_path):
     file_path_object = Path(file_path)
-
-    if not file_path_object.exists():
-        return
     
     if not file_path_object.is_file():
         return
@@ -19,8 +16,6 @@ def delete_file(file_path):
     # attempt to delete
     try:
         file_path_object.unlink()
-    except FileNotFound as e:
-        pass
     except:
         log.error(f'Failed to delete file at path {file_path} with error: {sys.exc_info()[0]}')
         
